01a09352135656b0014b74d18be7fa62fdf22789
Hackliu2008 <2404052713@qq.com>
2020-02-10 23:29:48 +0800

    Initial commit

---
+++ tools_me/up_pic.py
@@ -0,0 +1,21 @@
+import random
+
+from qiniu import Auth, put_file
+
+
+def up_photo(key, file_path, bucket_name):
+    access_key = 'KgHe4AAvPJStXOlhxGB3ds-3ndsUxS-wypBwKAgW'
+    secret_key = '6l1ujW79c4Zwo5XmpznDLTdQLaobW3As3r9fnol1'
+
+    q = Auth(access_key, secret_key)
+
+    token = q.upload_token(bucket_name, key, 3600)
+
+    ret, info = put_file(token, key, file_path)
+    # print(ret, info)
+
+    return info.status_code, ret.get('key')
+
+
+# status, url = up_photo('huihui.png', 'H:\world_pay\static\img\\banner.jpg', 'pay_pic')
+# print(status, url)
